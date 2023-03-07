814833835532b8fbd2a3782906b1c0b33a9600f8
Miguel <Miguel.M.Delarosa.-ND@disney.com>
2023-03-07 18:31:31 -0500

    Accept query param in api request

--- lib/repository/service_api.dart
+++ lib/repository/service_api.dart
@@ -4,8 +4,8 @@ import 'package:photo_app/models/photo_resource.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceApi {
-  Future<List<PhotoResource>> getPhotoResources() async {
-    Map<String, String> queryParams = {"query": "nature", "per_page": "10"};
+  getItemsPerCategory(String category) async {
+    Map<String, String> queryParams = {"query": category, "per_page": "10"};
     var url = Uri.https('api.pexels.com', 'v1/search', queryParams);
     Map<String, String> headers = {
       "Authorization":
@@ -15,7 +15,6 @@ class ServiceApi {
     Map<String, dynamic> map = jsonDecode(response.body);
     print('Response status: ${response.statusCode}');
     print('Response body: ${map["photos"]}');
-    return (map["photos"] as List).map((data) => PhotoResource.fromJson(data))
-        .toList();
+    return (map["photos"] as List).map((data) => PhotoResource.fromJson(data)).toList();
   }
 }
